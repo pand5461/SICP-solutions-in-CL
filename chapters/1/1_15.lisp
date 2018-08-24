52330099b6472c33a46be4710eef89f12277bb8f
pand5461 <pand5461@yandex.ru>
2018-08-25 00:13:56 +0300

    Added 1.15

---
+++ chapters/1/1_15.lisp
@@ -0,0 +1,19 @@
+;;; Exercise 1.15
+
+(defun square (x) (* x x))
+
+(defun p (x) (* x (- 3 (* 4 (square x)))))
+
+(defun sine (angle)
+  (if (<= (abs angle) 0.1)
+      angle
+      (p (sine (/ angle 3.0)))))
+
+(trace p)
+
+(sine 12.15)
+
+(untrace)
+
+;; from the trace, it is seen that #'p is called 5 times
+;; overall order of growth O(ln a), as log_3(a / 0.1) steps reduce a to a value around ~0.1
